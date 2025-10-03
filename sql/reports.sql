031c82a50d98c2a7fc4fbf151cfe8dc2a9bb964c
yves40 <yves.toubhans@free.fr>
2025-10-03 18:32:48 +0200

    Add field in books report

--- sql/reports.sql
+++ sql/reports.sql
@@ -6,9 +6,10 @@ select COUNT(bk_id) from books;
 
 select COUNT(distinct auth_fname, auth_lname)  from authors;
 
-select b.bk_title, a.auth_lname, e.ed_name, l.loc_city 
+select b.bk_title, a.auth_fname,  a.auth_lname, e.ed_name, l.loc_city 
   from books b, authors a, editors e, locations l
   where b.bk_location = l.loc_id
     and b.bk_editor = e.ed_id
     and b.bk_author = a.auth_id
   order by bk_title asc;
+  
